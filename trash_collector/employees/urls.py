7e6c515a177f4a10f54d2e1013079b224b686e50
SpencerMuniz <sbottomley44@gmail.com>
2021-10-27 11:55:52 -0700

    new method written and url link. Problems with github

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('new/', views.create, name="create"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
-    path('filter/', views.index, name="filter")
+    path('confirm_pickup_charge_balance/<int:customer_id>', views.confirm_pickup_charge_balance, name="confirm_pickup_charge_balance"),
 ]
